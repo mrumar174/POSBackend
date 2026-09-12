8cd6378eac11920620e0a2f206599eaf4350468e
Muhammad Umar <umarmian.mustafa@gmail.com>
2026-09-12 14:50:56 +0500

    Permissions

---
+++ POS.Web/Authorization/Authorization.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace POS.Web.Authorization
+{
+    public class PermissionRequirement : IAuthorizationRequirement
+    {
+        public string Permission { get; }
+        public PermissionRequirement(string permission) => Permission = permission;
+    }
+}
---
+++ POS.Web/Authorization/RequirePermissionAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace POS.Web.Authorization
+{
+    public class RequirePermissionAttribute : AuthorizeAttribute
+    {
+        public RequirePermissionAttribute(string permission) : base()
+        {
+            Policy = $"Permission:{permission}";
+        }
+    }
+}
